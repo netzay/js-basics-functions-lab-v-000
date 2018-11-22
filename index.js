cf19207b40a4d515cfb632a77ca00914ee6ca310
Ally Netzel <netzelally@gmail.com>
2018-11-22 01:18:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+  const hq = 42;
+  if (blocks > 42){
+    return blocks - hq;
+  } else {
+    return hq - blocks;
+  }
+}
+
+function distanceFromHqInFeet(feet){
+  const block = 264;
+  return block * distanceFromHqInBlocks(feet);
+}
+
+function distanceTravelledInFeet(start, end){
+  let distance = 0;
+  let feet = 264;
+  if (start > end){
+    distance = start - end;
+    return distance * feet;
+  } else {
+    distance = end - start;
+    return distance * feet;
+  }
+}
+
+function calculatesFarePrice(start, end){
+  if (distanceTravelledInFeet(start, end) >=400) ||(distanceTravelledInFeet(start, end)<= 2000){
+    
+  }
+  
+}
